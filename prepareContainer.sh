57bf0400d9abfc436df10817804b693541c8db76
athertahir <31277617+athertahir@users.noreply.github.com>
2019-12-22 16:07:12 +0500

    added

--- prepareContainer.sh
+++ prepareContainer.sh
@@ -12,4 +12,11 @@ pip install numpy==1.16.0
 
 pip install -U nltk
 pip install -U gensim
-#pip install --upgrade keras==2.0.8
+# pip install --upgrade keras==2.0.8
+
+
+pip install pydot
+
+conda install pydotplus -y
+conda install graphviz -y
+conda install python-graphviz -y
