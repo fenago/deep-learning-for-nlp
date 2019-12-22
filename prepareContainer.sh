f80fc8c5cae1e1c78cf8d9c9db6e4900af5eeff9
athertahir <31277617+athertahir@users.noreply.github.com>
2019-12-22 15:40:34 +0500

    added

---
+++ prepareContainer.sh
@@ -0,0 +1,15 @@
+# pip install --upgrade pip
+# pip install plotly 
+# pip install matplotlib_venn 
+# pip install pandas
+
+pip install pydotplus
+conda install graphviz -y
+
+echo 'Installing Dependencies'
+
+pip install numpy==1.16.0
+
+pip install -U nltk
+pip install -U gensim
+#pip install --upgrade keras==2.0.8
