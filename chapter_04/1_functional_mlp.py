61903f2a4703d6e7bdffc0e03d12b4c1ee4defc0
athertahir <31277617+athertahir@users.noreply.github.com>
2019-12-28 15:45:26 +0500

    added

--- chapter_04/1_functional_mlp.py
+++ chapter_04/1_functional_mlp.py
@@ -1,4 +1,14 @@
-# Multilayer Perceptron
+# %%
+'''
+## Multilayer Perceptron
+In this section, we define a Multilayer Perceptron model for binary classification. The model
+has 10 inputs, 3 hidden layers with 10, 20, and 10 neurons, and an output layer with 1 output.
+Rectified linear activation functions are used in each hidden layer and a sigmoid activation
+function is used in the output layer, for binary classification.
+'''
+
+# %%
+# example of a multilayer perceptron
 from keras.utils import plot_model
 from keras.models import Model
 from keras.layers import Input
@@ -9,6 +19,13 @@ hidden2 = Dense(20, activation='relu')(hidden1)
 hidden3 = Dense(10, activation='relu')(hidden2)
 output = Dense(1, activation='sigmoid')(hidden3)
 model = Model(inputs=visible, outputs=output)
+
+# %%
+''' 
+Running the example prints the structure of the network.
+'''
+
+# %%
 # summarize layers
 model.summary()
 # plot graph
